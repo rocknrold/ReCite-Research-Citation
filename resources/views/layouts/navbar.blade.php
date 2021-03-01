f63d0dd90ff5494b61925e40cfa5627c1942c8fc
rocknrold <bsitharold@gmail.com>
2021-03-01 23:59:25 +0800

    added words route and some features

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,7 +8,7 @@
         </ul>
         <ul class="right hide-on-med-and-down">
             <li><a class="waves-effect waves-teal btn black" href="{{ route('core.index') }}">Library<i class="material-icons right">local_library</i></a></li>
-            <li><a class="waves-effect waves-teal btn black">Words<i class="material-icons right">text_format</i></a></li>
+            <li><a class="waves-effect waves-teal btn black" href="{{ route('words.list') }}">Words<i class="material-icons right">text_format</i></a></li>
             <li><a class="waves-effect waves-teal btn black">Groups<i class="material-icons right">list</i></a></li>
             <li><a class="waves-effect waves-teal btn black">List<i class="material-icons right">dynamic_form</i></a></li>
             <li><a class="waves-effect waves-teal btn black">Docs<i class="material-icons right">article</i></a></li>
