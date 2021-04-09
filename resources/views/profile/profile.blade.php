ee9f27c62dbd51b545d8b86eb7708dd003618dda
rocknrold <bsitharold@gmail.com>
2021-04-09 15:41:33 +0800

    user profile view and edit page

---
+++ resources/views/profile/profile.blade.php
@@ -0,0 +1,55 @@
+@extends('welcome')
+@section('content')
+<div style="margin:auto;padding:5%;padding-top:10%;">
+    <div class="row">
+    @foreach($users as $user)
+            <h5><b>Profile</b></h5>
+            <div class="divider"></div><br>
+            <div class="row align-wrapper" style="width:200px;">
+              <img src="{{asset('images/no-profile.jpg')}}" alt="" class="circle responsive-img">
+            </div>
+            <div class="divider"></div>
+            <span><h6>Name</h6></span>
+                <div id="newUserName" hidden>
+                    <form action="#">
+                        <div class="input-field">
+                            <input id="username" type="search" required placeholder="New name">
+                            <i class="material-icons" id="dismissNameEdit">close</i>
+                        </div>
+                        <button id="nameSubmit" type="submit" class="btn" style="background-color:#3b5998">update</button>
+                    </form>
+                </div>
+                <div id="editNameDiv"> 
+                    <h5><span id="userNameEdit">{{$user->name}}</span>
+                        <span><a href="#!" id="btnNameEdit">
+                            <i class="tiny material-icons">mode_edit</i></a>
+                        </span>
+                        <span class="new badge blue" data-badge-caption="online"></span>
+                    </h5>
+                </div>
+            <div class="divider"></div>
+            <span><h6>Email</h6></span>
+                <div id="newUserEmail" hidden>
+                    <form action="#">
+                        <div class="input-field">
+                            <input id="useremail" type="search" required placeholder="New email" class="validate">
+                            <i class="material-icons" id="dismissEmailEdit">close</i>
+                        </div>
+                        <button id="emailSubmit" type="submit" class="btn" style="background-color:#3b5998">update</button>
+                    </form>
+                </div>
+            <div id="editEmailDiv">
+                <h5><span id="userEmailEdit">{{$user->email}}</span>
+                    <span><a href="#!" id="btnEmailEdit">
+                        <i class="tiny material-icons">mode_edit</i></a>
+                    </span>
+                </h5>
+            </div>
+            <div class="divider"></div>
+    @endforeach       
+    </div>
+</div>
+@endsection
+@section('scripts')
+    <script src="{{asset('js/profile.js')}}"></script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\CrwCoreController;
 use App\Http\Controllers\CrwWordController;
 use App\Http\Controllers\CrwCoresSearchController;
 use App\Http\Controllers\CrwCoreCitationController;
+use App\Http\Controllers\UserController;
 use App\Models\Crw_search;
 use Illuminate\Support\Facades\Http;
 
@@ -22,6 +23,9 @@ use Illuminate\Support\Facades\Http;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// HOME ROUTE SHOULD BE AVAILABLE TO PUBLIC BECAUSE IT IS ONLY THEIR CHOICE
+// IF THEY WANT TO HAVE PROFILE AND LIBRARY COLLECTIONS AND USE OTHER FEAUTRES
 Route::get('/', function () {
     $popular = Crw_search::popularSearches();
 
@@ -43,7 +47,7 @@ require __DIR__.'/auth.php';
 Route::get('/search?', [CrwSearchController::class, 'index'])->name('core.index');
 Route::get('/search', [CrwSearchController::class, 'search'])->name('core.search');
 
-
+// ROUTES THAT ARE PROTECTED IN THE APP
 Route::middleware(['direct.access'])->group(function () {
     Route::get('/search/library', [CrwCoreController::class, 'searchCoreLibrary']);
     Route::post('/search/library', [CrwCoreController::class, 'searchCoreLibrary'])->name('search.library');
@@ -56,9 +60,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/search/{word}/{id}', [CrwCoreController::class, 'nextPage'])->name('searchLibrary.next');
     Route::get('/search/{word}/{id}', [CrwCoreController::class, 'backPage'])->name('searchLibrary.back');
     Route::get('/core/like',[CrwCoresSearchController::class, 'corelikes'])->name('crw.likes');
+    Route::get('/profile/view',[CrwCoreController::class, 'profileView'])->name('profile.view');
 });
 
-
+// WITH AUTH MIDDLEWARE 
 Route::get('/add/library',[CrwCoresSearchController::class, 'addToLibrary'])->middleware('auth')->name('add.libary');
 Route::get('/groups',[CrwCoresSearchController::class, 'groupsIndex'])->middleware('auth')->name('crw.index');
 
@@ -67,6 +72,7 @@ Route::get('/citation/list',[CrwCoreCitationController::class,'citationList'])->
 
 Route::get('/word/list',[CrwWordController::class, 'showAll'])->name('words.list')->middleware('auth');
 
+// PUBLIC BECAUSE IT'S A DOCUMENTATION PAGE
 Route::get('/documentation', function(){
     return view('documentation');
 });
@@ -77,6 +83,9 @@ Route::get('/documentation', function(){
 Route::get('/api/GET/research/likes',[ApiController::class, 'getRL'])->name('getRL.api');
 
 
-Route::get('/profile/library/collections/',[CrwCoreController::class, 'profileLibraryCollections']);
+// USER ROUTES 
+Route::post('/profile/name', [UserController::class, 'nameUpdate'])->name('update.username');
+Route::post('/profile/email', [UserController::class, 'emailUpdate'])->name('update.useremail');
 
+Route::get('/profile/library/collections/',[CrwCoreController::class, 'profileLibraryCollections']);
 Route::post('/core/visible/{id}', [CrwCoreController::class, 'changeVisibility']);
